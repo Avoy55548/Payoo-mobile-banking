c4dc4fdc95b34a69a247139a6e139593290feacb
Tasin Islam Avoy <avoytasin@gmail.com>
2025-02-26 21:30:39 +0600

    Login Page java script done

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,16 @@
-document.getElementById("loginbtn").addEventListener("click", function (){
-    console.log("Login Done");
+document.getElementById("loginbtn").addEventListener("click", function (event){
+    event.preventDefault();
+    const AccountNumber= document.getElementById("account-num").value;
+    const pin= document.getElementById("pin").value;
+    if(AccountNumber.length==11){
+        if(pin==1234){
+            window.location.href="./main.html";
+        }
+        else{
+           alert("Invalid PIN");
+        }
+    }
+    else{
+        alert("Invalid Account");
+    }
 })
